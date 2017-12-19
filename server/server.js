541224bd78f0b5bd81e63b77ae9d15595823805a
Sumon Roy <roysuman088@gmail.com>
2017-12-19 17:04:08 +0530

    Emit newMessage or createMessage

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=> {
     console.log('New User connected');
 
+
+    socket.on('createMessage', (message)=> {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
