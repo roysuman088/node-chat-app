0a74f93c3aaa3fe0a6c1151096559261c4e43010
Sumon Roy <roysuman088@gmail.com>
2017-12-19 17:43:20 +0530

    Greet new User

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=> {
     console.log('New User connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat-app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text:'New User joined',
+        createAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message)=> {
         console.log('createMessage', message);
@@ -22,6 +32,12 @@ io.on('connection', (socket)=> {
             text: message.text,
             createAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
